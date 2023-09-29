5ea07fa37d1accdd905097d3baf836153a5fbad6
Park Jun Beom <junbeomb216@gmail.com>
2023-09-29 21:42:38 +0900

    First Commit

---
+++ SimpleGame/GSEGame.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Renderer.h"
+
+class GSEGame
+{
+public:
+
+	GSEGame(int windowSizeX, int windowSizeY);
+	~GSEGame();
+
+	void DrawAll();
+
+private:
+	Renderer * m_renderer;
+};
+
